93d2e40563f2594ba40de0f0412e8cee408d1dbc
nlmmaurya <nlmmaurya@gmail.com>
2018-12-03 00:35:11 +0530

    Hello World

---
+++ mycodes.py
@@ -0,0 +1,34 @@
+# x=int(input("Enter 1st num"))
+# y=int(input("Enter 2nd num"))
+# z=x+y
+# print(z)
+
+
+#ch=input("enter char")
+#print(ch)
+
+#ch=input("enter char")[0]
+#print(ch)
+
+#result=eval( input("enter expression"))
+#print(result)
+
+x=6
+r=x%2
+if r==0:
+    print("even")
+    if x>5:
+        print("greater")
+
+else:
+     print("odd")
+x=7
+
+if x==1:
+    print("one")
+
+elif x==2:
+    print("two")
+
+else:
+    print("else")
